906669d355b965900bff2b0b593570e199d0b66b
Anshuman Tiwari <anshumant72@gmail.com>
2023-03-22 00:37:08 +0530

    new commit

--- src/Components/Album.jsx
+++ src/Components/Album.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 export default function Album(props){
     return(
         <>
-          <Navbar title="TextUtils" mode={props.mode} toggleSwitch={props.toggleSwitch} />
+          <Navbar />
             <main>
                 <section className="py-5 text-center container">
                     <div className="row py-lg-5">
@@ -13,8 +13,8 @@ export default function Album(props){
                                 contents, the creator, etc. Make it short and sweet, but not too short so folks don’t
                                 simply skip over it entirely.</p>
                             <p>
-                                <a href="#" className="btn btn-primary my-2">Main call to action</a>
-                                <a href="#" className="btn btn-secondary my-2">Secondary action</a>
+                                <a href="/" className="btn btn-primary my-2">Main call to action</a>
+                                <a href="/" className="btn btn-secondary my-2">Secondary action</a>
                             </p>
                         </div>
                     </div>
--- src/index.js
+++ src/index.js
@@ -2,30 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-    createBrowserRouter, Router,
-    RouterProvider,
-} from "react-router-dom";
-import ErrorPage from "./Components/ErrorPage";
-import Album from "./Components/Album";
+// import Album from "./Components/Album";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-        errorElement:<ErrorPage/>
-    },
-    {
-        path:"/album",
-        element:<Album/>,
-        errorElement:<ErrorPage/>
-    }
-]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <App/>
   </React.StrictMode>
 );
 
